2c078467ade6e2180a00d94020edcb8f86ec37ea
Isaac de Medeiros <linzackmedeiros@gmail.com>
2026-09-04 17:50:56 -0300

    feat Adição de Exceptions personalizadas para facilitar as respostas do servidor

---
+++ backend/Exceptions/NotFoundException.cs
@@ -0,0 +1,7 @@
+﻿namespace OpenDoors.Api.Exceptions
+{
+    public class NotFoundException : Exception
+    {
+        public NotFoundException(string message) : base(message) { }
+    }
+}
---
+++ backend/Exceptions/ServerErrorException.cs
@@ -0,0 +1,7 @@
+﻿namespace OpenDoors.Api.Exceptions
+{
+    public class ServerErrorException : Exception 
+    {
+        public ServerErrorException(string message) : base(message) { }
+    }
+}
