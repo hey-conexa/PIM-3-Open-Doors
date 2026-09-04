a2e10a7f1affea34dd551e0a3e67295e54872c8c
Isaac de Medeiros <linzackmedeiros@gmail.com>
2026-09-04 17:50:31 -0300

    feat Adicao de Interfaces para desacoplação da API

---
+++ backend/Interfaces/Estudantes/IEstudanteRepository.cs
@@ -0,0 +1,15 @@
+﻿using OpenDoors.Api.DTOs;
+using OpenDoors.Api.Models;
+
+namespace OpenDoors.Api.Interfaces.Estudantes
+{
+    public interface IEstudanteRepository
+    {
+        public Task<List<Estudante>> ListarTodos();
+        public Task<List<Estudante>> ListarAtivos();
+        public Task<Estudante?> BuscarPorId(Guid id);
+        public Task Criar(Estudante novoEstudante);
+        public Task Atualizar(Estudante estudanteAtualizado);
+        public Task Deletar(Estudante estudante);
+    }
+}
---
+++ backend/Interfaces/Estudantes/IEstudanteService.cs
@@ -0,0 +1,14 @@
+﻿using OpenDoors.Api.DTOs;
+
+namespace OpenDoors.Api.Interfaces.Estudantes
+{
+    public interface IEstudanteService
+    {
+        public Task<IReadOnlyList<EstudanteDto>> ListarTodos();
+        public Task<IReadOnlyList<EstudanteDto>> ListarAtivos();
+        public Task<EstudanteDto> BuscarPorId(Guid id);
+        public Task<EstudanteDto> Criar(CreateEstudanteDto novoEstudante);
+        public Task<EstudanteDto> Atualizar(CreateEstudanteDto estudanteAtualizado);
+        public Task<EstudanteDto> Deletar(Guid id);
+    }
+}
